8d92823a27830c48b576839d4fae5ec8eb21f839
darkdilu <dilus1322@gmail.com>
2024-06-10 12:04:16 +0800

    initial commit for dashbaord

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './component/header'
+import Sidebar from './component/sidebar'
+import Home from './component/home'
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  )
+}
+
+export default App
